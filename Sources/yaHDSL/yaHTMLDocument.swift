83124db1c19273eca4bc4cae7bd16b054c011c38
Michael Redig <mredig@gmail.com>
2024-10-06 01:45:26 -0500

    (feat) a little progress

--- Sources/yaHDSL/yaHTMLDocument.swift
+++ Sources/yaHDSL/yaHTMLDocument.swift
@@ -1,5 +1,4 @@
 public struct yaHTMLDocument: Sendable {
-//	public var childNodes: [any yaHTMLNode]
 	public struct Context: Sendable {
 		public enum Mode: Sendable {
 			case minify
@@ -9,30 +8,29 @@ public struct yaHTMLDocument: Sendable {
 			public static let tabIndentation = Self.pretty(indentation: "\t")
 		}
 		let mode: Mode
+		var depth: Int = 0
+		var userInfo: [String: any Sendable]
 	}
 
-//	public let html: HTML
-//	public let context: Context
-//
-//	public init(html: HTML, context: Context) {
-//		self.html = html
-//		self.context = context
-//	}
-//
-//	public func render() throws -> String {
-//		try """
-//		<DOCTYPE html>
-//		\(html.render(withContext: context))
-//		"""
-//	}
+	public let html: HTML
+	public let context: Context
+
+	public init(html: HTML, context: Context) {
+		self.html = html
+		self.context = context
+	}
+
+	public func render() throws -> String {
+		try """
+		<DOCTYPE html>
+		\(html.render(withContext: context))
+		"""
+	}
 }
 
 public struct HTML: HTMLNode {
 	public var childNodes: [any HTMLNode]
 	public var tag: String? { "html" }
-	public mutating func addChildNode(_ child: any HTMLNode) {
-		childNodes.append(child)
-	}
 
 	public func render(withContext context: Context) throws -> String {
 		try childNodes.reduce(into: "") {
