983ab063211d66600a898903a9f28eb5029db78d
Michael Redig <mredig@gmail.com>
2024-10-06 16:44:44 -0500

    (feat) result builder test

--- Tests/yaHDSLTests/yaHDSLTests.swift
+++ Tests/yaHDSLTests/yaHDSLTests.swift
@@ -33,3 +33,74 @@ import yaHDSL
 	print(try doc.render())
 }
 
+
+@Test func basicHTMLBuilder() async throws {
+	let yes = true
+	let no = false
+
+	let optYes: String? = "optYes"
+	let optNo: String? = nil
+
+	let html = HTML {
+		Head {
+			"foooooo"
+		}
+
+		Body {
+			Div {
+				if yes {
+					"yar"
+				}
+				if let optYes {
+					optYes
+				}
+				if let optNo {
+					optNo
+				}
+
+				Div {
+					if yes {
+						"ifyes"
+					} else {
+						"ifyes bad"
+					}
+
+					if no {
+						"ifno bad"
+					} else {
+						"ifno"
+					}
+				}
+
+				"Foo"
+
+				"bar"
+
+
+			}
+		}
+	}
+
+	let doc = yaHTMLDocument(html: html, context: .defaultPretty)
+
+	let expectation = """
+		<DOCTYPE html>
+		<html>
+			<head>
+		foooooo
+			</head>
+			<body>
+				<div>
+		yar optYes
+					<div>
+		ifyes ifno
+					</div>
+		Foo bar
+				</div>
+			</body>
+		</html>
+		"""
+	let render = try doc.render()
+	print(render)
+	#expect(expectation == render)
+}
