3ff181cdde432b5942e547bcefa8fc1937c754b5
Michael Redig <mredig@gmail.com>
2024-10-07 01:03:56 -0500

    (feat) started individual tag tests

---
+++ Tests/yaHDSLTests/HTMLTagTests.swift
@@ -0,0 +1,37 @@
+import Testing
+import yaHDSL
+
+struct HTMLTagTests {
+
+	@Test func comment() throws {
+		let comment = Comment(content: "This is a comment.")
+
+		let render = try simpleRender(comment)
+
+		let expectation = "<!-- This is a comment. -->"
+		#expect(expectation == render)
+	}
+
+	@Test func a() async throws {
+		let anchor = A {
+			"Mah Link"
+		}
+		.withHref("/foo")
+		.withPing([
+			"www.google.com",
+			"www.bing.com"
+		])
+
+		let expected = "<a href=\"/foo\" ping=\"www.google.com www.bing.com\">Mah Link</a>"
+
+		let render = try simpleRender(anchor)
+		#expect(expected == render)
+	}
+
+	private func simpleRender<Component: HTMLNode>(_ component: Component) throws -> String {
+		let context = yaHTMLDocument.Context(mode: .minify, userInfo: [:])
+		return try component.render(withContext: context).with {
+			print($0)
+		}
+	}
+}
