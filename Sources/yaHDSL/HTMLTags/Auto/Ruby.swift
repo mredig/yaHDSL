11991dc5402fad56bac14584cc3e041522f270b9
Michael Redig <mredig@gmail.com>
2024-10-07 17:46:10 -0500

    (refactor) move html nodes into individual files

---
+++ Sources/yaHDSL/HTMLTags/Auto/Ruby.swift
@@ -0,0 +1,8 @@
+public struct Ruby: HTMLContentElement, GlobalAttributable, EventAttributable {
+	public var childNodes: [any HTMLNode]
+	public var attributes: [AttributeName: AttributeValue] = [:]
+	public var attributesOptions: AttributesOptions?
+	public init(childNodes: [any HTMLNode] = []) {
+		self.childNodes = childNodes
+	}
+}
