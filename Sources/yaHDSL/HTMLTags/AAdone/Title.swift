60891fd4b31a8485787ca0b6a46f336077b203bb
Michael Redig <mredig@gmail.com>
2024-10-08 03:54:42 -0500

    (feat) add HTMLTextContainer

--- Sources/yaHDSL/HTMLTags/AAdone/Title.swift
+++ Sources/yaHDSL/HTMLTags/AAdone/Title.swift
@@ -1,4 +1,4 @@
-public struct Title: HTMLNode, GlobalAttributable, EventAttributable {
+public struct Title: HTMLTextContainer, EventAttributable {
 
 	public var tag: String? { "title" }
 	public var attributes: [AttributeName: AttributeValue] = [:]
@@ -8,23 +8,5 @@ public struct Title: HTMLNode, GlobalAttributable, EventAttributable {
 
 	public init(_ text: String) {
 		self.text = text
-	}
-
-	public func render(withContext context: yaHTMLDocument.Context) throws -> String {
-		let openTag = {
-			if let attributeContent = renderAttributes() {
-				"<title \(attributeContent)>"
-			} else {
-				"<title>"
-			}
-		}()
-		let base = "\(openTag)\(text)</title>"
-		switch context.mode {
-		case .minify:
-			return base
-		case .pretty(let baseIndentation):
-			let indentation = String(repeating: baseIndentation, count: context.depth)
-			return "\(indentation)\(base)"
-		}
 	}
 }
---
+++ Sources/yaHDSL/Protocols/HTMLTextContainer.swift
@@ -0,0 +1,26 @@
+public protocol HTMLTextContainer: HTMLNode, GlobalAttributable {
+	var text: String { get set }
+
+	init(_ text: String)
+}
+extension HTMLTextContainer {
+	public func render(withContext context: Context) throws -> Output {
+		guard let tag else { return text }
+		let openTag = {
+			if let attributeContent = renderAttributes() {
+				"<\(tag) \(attributeContent)>"
+			} else {
+				"<\(tag)>"
+			}
+		}()
+		let closeTag = "</\(tag)>"
+		let base = "\(openTag)\(text)\(closeTag)"
+		switch context.mode {
+		case .minify:
+			return base
+		case .pretty(indentation: let indentation):
+			let totalIndentation = String(repeating: indentation, count: context.depth)
+			return "\(totalIndentation)\(base)"
+		}
+	}
+}
