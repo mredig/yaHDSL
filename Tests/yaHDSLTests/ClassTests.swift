3be12f471632f1dbbab0cdce1242527b2b95876a
Michael Redig <mredig@gmail.com>
2024-10-09 00:45:59 -0500

    (feat) some class attribute specific tests

---
+++ Tests/yaHDSLTests/ClassTests.swift
@@ -0,0 +1,59 @@
+import Testing
+import Foundation
+import yaHDSL
+
+struct ClassTests: SimpleTestRendering {
+
+	@Test("classAddRemove", arguments: [true, false]) func classAddRemove(keepIt: Bool) async throws {
+		let newDiv = Div {
+			Span {
+				let p = P("barfaroni")
+					.addClass("emphasize")
+				if keepIt {
+					p
+				} else {
+					p.removeClass("emphasize")
+				}
+			}
+		}
+
+		let rendered = try simpleRender(newDiv)
+
+		let expectationKeep = #"<div><span><p class="emphasize">barfaroni</p></span></div>"#
+		let expectationDont = #"<div><span><p>barfaroni</p></span></div>"#
+
+		if keepIt {
+			#expect(expectationKeep == rendered)
+		} else {
+			#expect(expectationDont == rendered)
+		}
+	}
+
+	@Test("classAddRemoveNoNullifyEmpty", arguments: [true, false]) func classAddRemove2(keepIt: Bool) async throws {
+		let newDiv = Div {
+			Span {
+				let p = P("barfaroni")
+					.addClass("emphasize")
+					.customizingOptions {
+						$0?.nullifyEmptyAttributes = false
+					}
+				if keepIt {
+					p
+				} else {
+					p.removeClass("emphasize")
+				}
+			}
+		}
+
+		let rendered = try simpleRender(newDiv)
+
+		let expectationKeep = #"<div><span><p class="emphasize">barfaroni</p></span></div>"#
+		let expectationDont = #"<div><span><p class="">barfaroni</p></span></div>"#
+
+		if keepIt {
+			#expect(expectationKeep == rendered)
+		} else {
+			#expect(expectationDont == rendered)
+		}
+	}
+}
