24cb44957d5783c1558629482c768283a5b707be
Michael Redig <mredig@gmail.com>
2025-01-06 20:53:43 -0600

    (refactor) custom html node throws

--- Sources/yaHDSL/Protocols/CustomHTMLNode.swift
+++ Sources/yaHDSL/Protocols/CustomHTMLNode.swift
@@ -1,11 +1,11 @@
 public protocol CustomHTMLNode: HTMLNode {
 	associatedtype RootNode: HTMLNode
 
-	var node: RootNode { get }
+	var node: RootNode { get throws }
 }
 
 extension CustomHTMLNode {
-	public var tag: String? { node.tag }
+	public var tag: String? { try? node.tag }
 
 	public func render(withContext context: Context) throws -> Output {
 		try node.render(withContext: context)
